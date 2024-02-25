860e5b7b5f7e28ab1794c67baa1a6e12a2f1dfff
Eren GÜN <erengun4@gmail.com>
2024-02-25 13:19:58 +0300

    Update projects in projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,21 +7,44 @@ export type Project = {
 
 const projects: Project[] = [
   {
-    title: "Dictionary App",
-    techs: ["ReactJS (NextJS)", "react-query", "zod"],
-    link: "https://github.com/MaeWolff/dictionary-app",
+    title: "Flutter Riverpod 2.0 Template",
+    techs: ["Flutter", "Riverpod", "Freezed", "Go Router", "Dio", "Hive", "GetIt"],
+    link: "https://github.com/Erengun/Flutter-Riverpod-2.0-Template",
   },
   {
-    title: "Portfolio / Lina BLIDI",
-    techs: ["ReactJS (NextJS)", "TypeScript"],
-    link: "https://www.linablidi.fr/",
+    title: "Pati Pati App",
+    techs: ["Flutter", "Firebase", "Riverpod", "Freezed", "Go Router", "Dio", "Hive", "GetIt"],
+    link: "https://play.google.com/apps/testing/app.patipati",
   },
   {
-    title: "Portfolio / Template",
-    techs: ["Astro"],
+    title: "Tinywall",
+    techs: ["Flutter", "MobX", "Hive", "Dio", "GetIt", "MVVM", "Clean Architecture",],
+    link: "https://tinywall.app",
+  },
+ 
+ 
+  {
+    title: "Grog app",
+    techs: ["Flutter", "Firebase", "Cloud Firestore", "Firebase Auth", "Firebase Functions", "Riverpod"],
     link: "/",
     isComingSoon: true,
   },
+  {
+    title: "GOHouse - Mobile app",
+    techs: ["Flutter", "Firebase", "Cloud Firestore", "Firebase Auth", "GetIt"],
+    link: "/",
+  },
+  
+  {
+    title: "Firebase Flutter Snippets",
+    techs: ["Flutter", "Firebase", "Cloud Firestore", "Firebase Auth", "Firebase Functions"],
+    link: "https://github.com/Erengun/snippets-flutter"
+  },
+  {
+    title: "Browser Image Compression - Flutter Package",
+    techs: ["Flutter", "Dart", "Image Compression"],
+    link: "https://github.com/ChampionsGoal/browser_image_compression",
+  }
 ];
 
 export default projects;
